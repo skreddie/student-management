52cab016b8f8b737e8db67b98a1030e48fae7211
Sai Kumar Reddy <saikumarreddy648@gmail.com>
2026-01-07 19:01:33 +0530

    Add springboot validator dependency

---
+++ src/main/java/com/example/studentmanagement/dto/StudentRequestDTO.java
@@ -0,0 +1,22 @@
+package com.example.studentmanagement.dtos;
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StudentRequestDTO {
+    @NotNull(message = "email is required ")
+    @Email
+    private String email;
+
+    @NotNull(message = "name is required")
+    private String name;
+
+    @NotNull(message = "course is required")
+    private String course;
+}
